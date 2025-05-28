41a7a9193634122255085c2087104042765cc607
FabricioEllerSL <fabricioellersl@gmail.com>
2025-05-28 00:07:23 -0300

    :wrench: fix: fixed url error

--- estoque/urls.py
+++ estoque/urls.py
@@ -5,7 +5,7 @@ app_name = 'estoque'
 
 urlpatterns = [
     path('', views.display_products, name='display_products'),
-    path('/create', views.create_product, name='create_product'),
-    path('/update/<int:id>', views.update_product, name='update_product'),
-    path('/delete/<int:id>', views.delete_product, name='delete_product'),
+    path('create', views.create_product, name='create_product'),
+    path('update/<int:id>', views.update_product, name='update_product'),
+    path('delete/<int:id>', views.delete_product, name='delete_product'),
 ]
